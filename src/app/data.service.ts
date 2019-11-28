f7e531b2f8662f93de115133e79ac2564b8dbeef
priyanka <priya.bomble29@gmail.com>
2019-11-28 23:20:59 +0530

    new

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,22 +10,22 @@ export class DataService {
 
   select()
   {
-    return this.helper.get("http://52.66.211.61:8888/emps");
+    return this.helper.get("http://localhost:8888/emps");
   }
   SelectByNo(No)
   {
-    return this.helper.get("http://52.66.211.61:8888/emps/" +No);
+    return this.helper.get("http://localhost:8888/emps/" +No);
   }
   Update(empobj)
   {
-    return this.helper.put("http://52.66.211.61:8888/emps/"+ empobj.No,empobj);
+    return this.helper.put("http://localhost:8888/emps/"+ empobj.No,empobj);
   }
   Delete(No)
   {
-    return this.helper.delete("http://52.66.211.61:8888/emps/" + No);
+    return this.helper.delete("http://localhost:8888/emps/" + No);
   }
   Insert(empobj)
   {
-    return this.helper.post("http://52.66.211.61:8888/emps" ,empobj);
+    return this.helper.post("http://localhost:8888/emps" ,empobj);
   }
 }
